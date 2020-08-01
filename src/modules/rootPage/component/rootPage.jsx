99fb23b455f59e640d6ace90463a111b4fd7019c
KPVBY <kpvbygm@gmail.com>
2020-08-01 03:10:44 +0300

    refactoring current data

--- src/modules/rootPage/component/rootPage.jsx
+++ src/modules/rootPage/component/rootPage.jsx
@@ -5,13 +5,17 @@ import { Calendar } from "react-calendar";
 export const RootPage = ({history}) => {
 
     const clickDay = event => history.push(`/${event.getFullYear()}-${event.getMonth()+1}-${event.getDate()}`);
+    const currentDay = new Date();
+    const year = currentDay.getFullYear();
+    const month = currentDay.getMonth();
+    const day = currentDay.getDate();
 
     return (
         <div className="root-page">
             <Calendar
                 showNeighboringMonth={false}
                 onClickDay={event => clickDay(event)}
-                prev2Label={null} next2Label={null} maxDate={(new Date(2020, 11, 31))}
+                prev2Label={null} next2Label={null} maxDate={new Date(year,month,day-1)}
                 minDate={(new Date(2020, 0, 1))}
             />
         </div>
